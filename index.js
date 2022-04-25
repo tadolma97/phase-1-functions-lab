ba6fce4f25ff4bbfaa6a76fe8b76347d3f8015c8
Tashi Dolma <tadolma97@gmail.com>
2022-04-25 17:33:04 -0400

    commit tree a8b1f86388fb1b92bdca8d7faeaed70ca3f8fa21

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickUp){
+    return Math.abs(42-pickUp)
+}
+function distanceFromHqInFeet(pickUp){
+    return distanceFromHqInBlocks(pickUp)*264
+}
+
+function distanceTravelledInFeet(start, destination){
+    return (Math.abs(start-destination))*264
+}
+
+function calculatesFarePrice(start, destination){
+    if (distanceTravelledInFeet(start, destination)<=400){
+        return 0;
+    }
+    else if (distanceTravelledInFeet(start, destination)>400 && distanceTravelledInFeet(start, destination)<=2000){
+        return (distanceTravelledInFeet(start, destination)-400)*.02;
+    }
+    else if (distanceTravelledInFeet(start, destination)>2000 && distanceTravelledInFeet(start, destination)<=2500){
+        return 25
+    }
+    else if (distanceTravelledInFeet(start, destination)>2500){
+        return "cannot travel that far"
+    }
+}
